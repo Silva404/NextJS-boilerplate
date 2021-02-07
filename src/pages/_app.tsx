c7e6bb2cc028b8675ed471ceddfa6d60fc1f6aec
Silva404 <brenosilva0252@gmail.com>
2021-02-07 16:06:00 -0300

    feat: add PWA to boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import GlobalStyles from 'styles/global'
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import GlobalStyles from "styles/global"
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate NextJS</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="manifest.json" />
         <meta name="description" content="Learning NextJS" />
       </Head>
       <GlobalStyles />
